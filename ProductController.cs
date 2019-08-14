3153ebbe4ae51afea7c752883ebd2a5c304cb9a2
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-08-15 00:11:31 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,45 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductModel pro = new ProductModel();
+        DataAccessLayer objproduct = new DataAccessLayer();
+       [HttpGet]
+        public ActionResult AddtoCart()
+        {
+            return View(pro);
+        }
+        [HttpPost]
+        public ActionResult AddtoCart(int? id)
+        {
+            return View();
+        }
+
+
+
+
+      
+
+
+
+
+
+
+
+        public ActionResult GetAllProducts()
+        {
+            List<ProductModel> lstProducts = new List<ProductModel>();
+            lstProducts = objproduct.GetAllProducts().ToList();
+
+            return View();
+        }
+        
+    }
+}
